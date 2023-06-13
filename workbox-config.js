2706f316f5b1233958134612a32198c19740b59d
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-06-13 12:16:49 +0700

    test: support pwa

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "wiki/",
+  "globPatterns": [
+    "**/*.{css,js,html,png,eot,svg,ttf,webp,webm,woff,woff2,json}"
+  ],
+  "swDest": "wiki/sw.js",
+  "swSrc": "sw.js"
+};
